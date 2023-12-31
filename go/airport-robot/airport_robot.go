a331b6181931ac7a58608e7f768be51cbbf6ddc2
Manuel Martin <manuel.martin@gmail.com>
2023-12-31 14:01:42 +0000

    feat: add airport-robot exercise

---
+++ go/airport-robot/airport_robot.go
@@ -0,0 +1,36 @@
+package airportrobot
+
+import (
+	"fmt"
+)
+
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+func SayHello(name string, greeter Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s", greeter.LanguageName(), greeter.Greet(name))
+}
+
+type Italian struct {
+}
+
+func (italian Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (italian Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s!", name)
+}
+
+type Portuguese struct {
+}
+
+func (portuguese Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (portuguese Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Olá %s!", name)
+}

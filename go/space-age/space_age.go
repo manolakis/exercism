70eb44d46a0bd92d65459b203d896a498df0793b
Manuel Martin <manuel.martin@gmail.com>
2024-01-02 10:09:34 +0000

    feat: add space-age exercise

---
+++ go/space-age/space_age.go
@@ -0,0 +1,22 @@
+package space
+
+// Planet represents a planet
+type Planet string
+
+const oneEarthYear float64 = 31_557_600
+
+var years = map[Planet]float64{
+	"Earth":   oneEarthYear,
+	"Mercury": oneEarthYear * 0.2408467,
+	"Venus":   oneEarthYear * 0.61519726,
+	"Mars":    oneEarthYear * 1.8808158,
+	"Jupiter": oneEarthYear * 11.862615,
+	"Saturn":  oneEarthYear * 29.447498,
+	"Uranus":  oneEarthYear * 84.016846,
+	"Neptune": oneEarthYear * 164.79132,
+}
+
+// Age returns how many ages represents some seconds in a planet
+func Age(seconds float64, planet Planet) float64 {
+	return seconds / years[planet]
+}

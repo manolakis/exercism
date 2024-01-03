f237f8592aed2c68a76cfab86c4c41d0d7a64d4d
Manuel Martin <manuel.martin@gmail.com>
2024-01-03 17:15:18 +0000

    feat: add pangram exercise

---
+++ go/pangram/pangram.go
@@ -0,0 +1,15 @@
+package pangram
+
+import "strings"
+
+func IsPangram(input string) bool {
+	input = strings.ToLower(input)
+
+	for char := 'a'; char <= 'z'; char++ {
+		if !strings.ContainsRune(input, char) {
+			return false
+		}
+	}
+
+	return true
+}
